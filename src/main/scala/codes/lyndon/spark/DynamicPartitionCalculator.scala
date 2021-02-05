111eff6f904884ca8b6966c899f6b0cfc30a9066
Lyndon Armitage <lyndon.armitage@gmail.com>
2021-02-05 16:06:31 +0000

    Complete refactor of DynamicPartitionCalculator

--- src/main/scala/codes/lyndon/spark/DynamicPartitionCalculator.scala
+++ src/main/scala/codes/lyndon/spark/DynamicPartitionCalculator.scala
@@ -12,29 +12,67 @@ import scala.math.{max, min}
   */
 object DynamicPartitionCalculator {
 
+  @transient
   private[this] val logger: Logger = LoggerFactory.getLogger(getClass)
 
-  case class PartitionRecommendations(
-      estimatedTotalSize: BigInt,
-      maxSizePerPartition: Long,
-      recommendedPartitionCount: Int,
-      alignedRecommendedPartitionCount: Int
-  )
+  /**
+    * 128MiB in Bytes
+    */
+  val _128MiB: Long = 128L * 1024L * 1024L
 
   /**
-    * Calculate the partitions required for a given DataFrame using the given
-    * options. This method invokes [[guessPartitionCount()]] see that method
-    * for more details on how this is achieved.
-    *
-   * @param dataFrame The DataFrame to calculate the partitions count for
-    * @param options The options to use
-    * @return The estimated dynamic partition count
+    * Default option values
     */
-  def apply(
-      dataFrame: DataFrame,
-      options: DynamicPartitionOptions = DynamicPartitionOptions()
-  ): PartitionRecommendations =
-    guessPartitionCount(dataFrame, options)
+  object Default {
+
+    /**
+      * 100,000 partitions seems reasonable as when combined with the above
+      * default we'd be able to handle ~12 terabytes of data.
+      * With smaller values it would work relatively okay as well.
+      */
+    val maxTotalPartitions: Int = 100000
+
+    /**
+      * When dealing with smaller data sets a lower limit of 80 partitions should
+      * ensure we still get decent parallelism, e.g. machines with 16 cores will
+      * be given 5 tasks.
+      */
+    val minTotalPartitions: Int = 16
+
+    /**
+      * Default to the number of available processors
+      */
+    val executorsPerNode: Int = Runtime.getRuntime.availableProcessors()
+
+    /**
+      * Defaults to 1, assuming a single node
+      */
+    val clusterNodes: Int = 1
+
+    /**
+      * A reasonable sample size to use when sampling a DataFrame, generally if
+      * a DataFrame is small enough we can sample the whole thing
+      */
+    val sampleSize: Int = 10000
+  }
+
+  /**
+    * Recommendations for how to size partitions
+    * @param estimatedTotalSize The estimated total size of the data in bytes
+    * @param maxSizePerPartitionBytes The rough max size per partition in bytes
+    * @param recommendedPartitionCount The recommended count of partitions
+    * @param alignedRecommendedPartitionCount The recommended count of
+    *                                         partitions aligned with the count
+    *                                         of executors
+    * @param totalExecutors The total count of executors
+    */
+  case class PartitionRecommendations(
+      estimatedTotalSize: Long,
+      maxSizePerPartitionBytes: Long,
+      recommendedPartitionCount: Int,
+      alignedRecommendedPartitionCount: Int,
+      totalExecutors: Int
+  )
 
   /**
     * <p>
@@ -52,28 +90,34 @@ object DynamicPartitionCalculator {
     * </p>
     *
     * @param dataFrame The DataFrame to calculate the partitions count for
-    * @param options The options to use
     * @param sampleSize The sample size to use for estimating row sizes
+    * @param maxSizePerPartitionBytes The max size per partition,
+    *                                 normally good to use something around 128MiB
+    * @param maxTotalPartitions The maximum amount of partitions to recommend
+    * @param minTotalPartitions The minimum amount of partitions to recommend,
+    *                           should be a multiple of the amount of executors generally
+    * @param executorsPerNode The number of executors per node in the cluster
+    * @param clusterNodes The number of cluster nodes
     * @return The estimated dynamic partition count
     */
-  def guessPartitionCount(
+  def recommendForDataFrame(
       dataFrame: DataFrame,
-      options: DynamicPartitionOptions,
-      sampleSize: Int = 10000
+      sampleSize: Int = 10000,
+      maxSizePerPartitionBytes: Long = _128MiB,
+      maxTotalPartitions: Int = Default.maxTotalPartitions,
+      minTotalPartitions: Int = Default.minTotalPartitions,
+      executorsPerNode: Int = Default.executorsPerNode,
+      clusterNodes: Int = Default.clusterNodes
   ): PartitionRecommendations = {
-    // Convert the given user options into our own internal vars for use
-    val DynamicPartitionOptions(
-      maxSizePerPartition,
-      maxTotalPartitions,
-      minTotalPartitions,
-      executorsPerNode,
-      clusterNodes
-    ) = options
+
+    if (sampleSize < 1)
+      throw new IllegalArgumentException(
+        s"sampleSize must be greater than 0, was $sampleSize"
+      )
 
     val currentPartitions = dataFrame.rdd.getNumPartitions
     logger.debug(s"Currently there are $currentPartitions partitions")
 
-    logger.trace(s"Calculating row count")
     // Could potentially use rdd.countApprox if we need this result quick,
     // However countApprox will still use up resources after we get the initial
     // value
@@ -81,11 +125,12 @@ object DynamicPartitionCalculator {
     logger.debug(s"$totalRowCount total rows")
 
     val rowSize = getRowSize(dataFrame, sampleSize, totalRowCount)
+    logger.debug(s"Row size is $rowSize")
 
     recommend(
       totalRowCount,
       rowSize,
-      maxSizePerPartition,
+      maxSizePerPartitionBytes,
       maxTotalPartitions,
       minTotalPartitions,
       executorsPerNode,
@@ -94,20 +139,36 @@ object DynamicPartitionCalculator {
 
   }
 
+  /**
+    * Make a recommendation on the amount of partitions that should be used for
+    * a given stage of a job based on the amount of data being processed, shape
+    * of it and the fixed cluster size.
+    *
+   * @param totalRowCount The total count of rows
+    * @param rowSizeBytes The size of a single row in bytes
+    * @param maxSizePerPartitionBytes The max size per partition,
+    *                                 normally good to use something around 128MiB
+    * @param maxTotalPartitions The maximum amount of partitions to recommend
+    * @param minTotalPartitions The minimum amount of partitions to recommend,
+    *                           should be a multiple of the amount of executors generally
+    * @param executorsPerNode The number of executors per node in the cluster
+    * @param clusterNodes The number of cluster nodes
+    * @return
+    */
   def recommend(
       totalRowCount: Long,
-      rowSize: Long,
-      maxSizePerPartition: Long,
+      rowSizeBytes: Long,
+      maxSizePerPartitionBytes: Long = _128MiB,
       maxTotalPartitions: Int,
       minTotalPartitions: Int,
       executorsPerNode: Int,
       clusterNodes: Int
   ): PartitionRecommendations = {
-    val totalExecutors = executorsPerNode * clusterNodes
-    val totalSize      = BigInt(totalRowCount) * rowSize
+    val totalExecutors: Int = executorsPerNode * clusterNodes
+    val totalSize: Long     = totalRowCount * rowSizeBytes
 
     val recommendedPartitionCount =
-      (totalSize / maxSizePerPartition).toInt
+      (totalSize / maxSizePerPartitionBytes).toInt
 
     val newPartitions = max(
       min(
@@ -123,9 +184,10 @@ object DynamicPartitionCalculator {
 
     PartitionRecommendations(
       totalSize,
-      maxSizePerPartition,
+      maxSizePerPartitionBytes,
       newPartitions,
-      alignedNewPartitions
+      alignedNewPartitions,
+      totalExecutors
     )
   }
 
