c379a86c2ea5db2320652553668855309df19bf7
Lyndon Armitage <lyndon.armitage@gmail.com>
2021-02-18 14:38:11 +0000

    Improve densify_on_date

--- src/main/scala/codes/lyndon/spark/ExtraDataFrameFunctions.scala
+++ src/main/scala/codes/lyndon/spark/ExtraDataFrameFunctions.scala
@@ -1,14 +1,18 @@
 package codes.lyndon.spark
 
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.LyndonFunctions.dates_between
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{Column, DataFrame}
 
 object ExtraDataFrameFunctions {
 
   implicit class DataFrameUtils(df: DataFrame) {
 
-    def densify_on_date(dateColumn: String): DataFrame = {
+    def densify_on_date(
+        dateColumn: String,
+        ascending: Boolean = true,
+        nullsFirst: Boolean = true
+    ): DataFrame = {
       import df.sparkSession.implicits._
       val dates = df
         .select(dateColumn)
@@ -21,9 +25,15 @@ object ExtraDataFrameFunctions {
         .withColumn(dateColumn, explode($"range"))
         .drop("range")
 
+      val sortCol = (ascending, nullsFirst) match {
+        case (true, true)   => col(dateColumn).asc_nulls_first
+        case (true, false)  => col(dateColumn).asc_nulls_last
+        case (false, true)  => col(dateColumn).desc_nulls_first
+        case (false, false) => col(dateColumn).desc_nulls_last
+      }
       // Must be outer to include any rows with null values in their date column
       df.join(dates, Seq(dateColumn), "outer")
-        .sort(col(dateColumn).asc_nulls_first)
+        .sort(sortCol)
     }
   }
 
