7c33a747e28e61fbef7d7394d90e38e33ab80612
Lyndon Armitage <lyndon.armitage@gmail.com>
2021-11-08 12:45:10 +0000

    Fix OutputWriter to work with V1 Spark writer

--- src/main/scala/codes/lyndon/spark/job/OutputWriter.scala
+++ src/main/scala/codes/lyndon/spark/job/OutputWriter.scala
@@ -2,7 +2,7 @@ package codes.lyndon.spark.job
 
 import codes.lyndon.spark.{ExternalCatalogHelper, RecordCountListener}
 import org.apache.spark.sql.functions.lit
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.slf4j.LoggerFactory
 
 import scala.util.{Failure, Success, Try}
@@ -13,18 +13,26 @@ object OutputWriter {
 
   sealed abstract class WriteMode {
     def stringValue: String
+    def sparkMode: SaveMode
 
     override def toString: String = stringValue
   }
 
   case object Overwrite extends WriteMode {
-    override def stringValue: String = "overwrite"
+    override val stringValue: String = "overwrite"
+    override val sparkMode: SaveMode = SaveMode.Overwrite
   }
   case object OverwriteOnly extends WriteMode {
-    override def stringValue: String = "overwrite_only"
+    override val stringValue: String = "overwrite_only"
+    override val sparkMode: SaveMode = SaveMode.Overwrite
   }
   case object CreateOnly extends WriteMode {
-    override def stringValue: String = "create"
+    override val stringValue: String = "create"
+    override val sparkMode: SaveMode = SaveMode.ErrorIfExists
+  }
+  case object Append extends WriteMode {
+    override val stringValue: String = "append"
+    override val sparkMode: SaveMode = SaveMode.Append
   }
 
   final case class WriteFailuresException(
@@ -41,7 +49,7 @@ object OutputWriter {
 
   object WriteMode {
     private val modes: Seq[WriteMode] =
-      Seq(Overwrite, OverwriteOnly, CreateOnly)
+      Seq(Overwrite, OverwriteOnly, CreateOnly, Append)
 
     val validModesString: String = modes.map(_.stringValue).mkString(", ")
 
@@ -116,27 +124,31 @@ object OutputWriter {
       val format      = table.format
       val partitionBy = table.partitionBy.map(lit(_))
       val path        = table.source.locationURI.resolve(table.name).toString
+      val writeToDB = table.source.`type` match {
+        case JDBC => true
+        case _    => false
+      }
 
       logger.info(s"Writing $tableDbString to $path as $format in mode $mode")
 
-      var partial = df
-        .writeTo(tableDbString)
-        .using(format)
+      var partial = df.write
+        .format(format)
         .option("path", path)
 
       if (partitionBy.nonEmpty) {
         logger.info(
           s"Partitioning $tableDbString by ${table.partitionBy.mkString(", ")}"
         )
-        partial = partial.partitionedBy(partitionBy.head, partitionBy.tail: _*)
+        partial = partial.partitionBy(table.partitionBy: _*)
       }
 
       logger.debug(s"Starting write for $tableDbString")
       try {
-        mode match {
-          case Overwrite     => partial.createOrReplace()
-          case OverwriteOnly => partial.replace()
-          case CreateOnly    => partial.create()
+        partial = partial.mode(mode.sparkMode)
+        if (writeToDB) {
+          partial.saveAsTable(tableDbString)
+        } else {
+          partial.save()
         }
       } catch {
         case e: Throwable =>
@@ -160,14 +172,19 @@ object OutputWriter {
           bytesWritten = Some(bytes)
         )
 
-        ExternalCatalogHelper.updateStats(
-          table.source.name,
-          table.name,
-          stats
-        ) match {
-          case Failure(cause) =>
-            logger.error(s"Failed to write stats for $tableDbString", cause)
-          case Success(_) =>
+        if (writeToDB) {
+          ExternalCatalogHelper.updateStats(
+            table.source.name,
+            table.name,
+            stats
+          ) match {
+            case Failure(cause) =>
+              logger.error(
+                s"Failed to write stats to external catalog for $tableDbString",
+                cause
+              )
+            case Success(_) =>
+          }
         }
 
         LineageStatistics.from(stats) match {
